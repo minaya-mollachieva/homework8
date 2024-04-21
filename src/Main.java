551f810ef09786182ececdc69c8eff0b6a37ac51
Minaya <minaya.gadzhieva05@mail.ru>
2024-04-21 23:27:12 +0300

    исправленное дз

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Задание 1");
@@ -19,51 +21,56 @@ public class Main {
 
         for (int i = 0; i < m1.length; i++) {
             if (i != m1.length - 1) {
-                System.out.println(m1[i] + ", ");
+                System.out.print(m1[i] + ", ");
             } else {
-                System.out.println(m1[i]);
+                System.out.print(m1[i]);
             }}
+        System.out.println();
             for (int i = 0; i < m2.length; i++) {
                 if (i != m2.length - 1) {
-                    System.out.println(m2[i] + ", ");
+                    System.out.print(m2[i] + ", ");
                 } else {
-                    System.out.println(m2[i]);
+                    System.out.print(m2[i]);
                 }}
+        System.out.println();
         for (int i = 0; i < m3.length; i++) {
             if (i != m3.length - 1) {
-                System.out.println(m3[i] + ", ");
+                System.out.print(m3[i] + ", ");
             } else {
-                System.out.println(m3[i]);
+                System.out.print(m3[i]);
             }
         }
-
+        System.out.println();
             System.out.println("Задание 3");
 
         for (int i = m1.length-1;i>=0; i--) {
-            if (i != m1.length - 1) {
-                System.out.println(m1[i] + ", ");
+            if (i !=0) {
+                System.out.print(m1[i] + ", ");
             } else {
-                System.out.println(m1[i]);
+                System.out.print(m1[i]);
             }}
+        System.out.println();
         for (int i = m2.length-1;i>=0; i--) {
-            if (i != m2.length - 1) {
-                System.out.println(m2[i] + ", ");
+            if (i != 0) {
+                System.out.print(m2[i] + ", ");
             } else {
-                System.out.println(m2[i]);
+                System.out.print(m2[i]);
             }}
+        System.out.println();
         for (int i = m3.length-1;i>=0; i--) {
-            if (i != m3.length - 1) {
-                System.out.println(m3[i] + ", ");
+            if (i !=0) {
+                System.out.print(m3[i] + ", ");
             } else {
-                System.out.println(m3[i]);
+                System.out.print(m3[i]);
             }
         }
+        System.out.println();
         System.out.println("Задание 4");
         for (int i = 0; i < m1.length; i++) {
             if (m1[i]%2!=0) {
                 m1[i]=m1[i]+1;
+                System.out.println(Arrays.toString(m1));
 
-                System.out.println(m1[i] + " ");
             }
     }
     }
